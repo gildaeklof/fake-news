5e11a466309b80744feda7d91836f06a1fc8608b
Gilda Eklof <gilda@eklof.se>
2020-10-19 11:51:20 +0200

    added content

--- data.php
+++ data.php
@@ -2,5 +2,12 @@
 
 declare(strict_types=1);
 
+$authors = [
+    ['name' => 'Gilda Eklöf', 'id' => 1],
+    ['name' => 'Kanye West', 'id' => 2],
+    ['name' => 'Kim Kardashian', 'id' => 3],
+    ['name' => 'Bill Gates', 'id' => 4],
+    ['name' => 'Leif G.W. Persson', 'id' => 5]
+];
 // This is the file where you can keep your data arrays such as articles and
 // authors.
