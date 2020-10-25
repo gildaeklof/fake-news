7223962ebfe0ab5c0e5f085fc0ee979932d738ba
Gilda Eklof <gilda@eklof.se>
2020-10-25 17:55:57 +0100

    added function

--- functions.php
+++ functions.php
@@ -2,6 +2,19 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/data.php';
+
+function getAuthorById(int $articles_id, array $authors): string
+{
+
+    foreach ($authors as $author) {
+
+        if ($author['id'] === $articles_id) {
+
+            return $author['author'];
+        }
+    }
+}
 
 // This is the file where you can keep all your functions. Remember to NOT
 // execute/run any functions in this file. Keep it dumb.
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . '/data.php';
+
 require __DIR__ . '/functions.php';
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
@@ -17,33 +18,24 @@ require __DIR__ . '/functions.php';
 </head>
 
 <body>
+    <section class="container">
 
-    <?php foreach ($articles as $article) :
-        $title = $article['title'];
-        $content = $article['content'];
-        $published_date = $article['published_date'];
-        $img = $article['img'];
-        $likes = $article['likes'];
+        <?php foreach ($articles as $article) : ?>
+            <article>
+                <div class="container">
+                    <h3><?= $article['title']; ?></h3>
+                    <img src="<?= $article['img']; ?>">
+                    <p class="content"><?= $article['content']; ?></p>
+                    <p><?= $article['published_date']; ?></p>
+                    <p><?= 'Posted by: ' . getAuthorById($article['id'], $authors); ?></p>
+                    <p><?= $article['likes']; ?></p>
+                </div>
+            </article>
 
-        $firstName = $authors[$article['authorId']]['firstName'];
-        $lastName = $authors[$article['authorId']]['lastName'];
-        $name = "$firstName $lastName";
-    ?>
+        <?php endforeach; ?>
 
-        <article>
+    </section>
 
-            <div class="article">
-                <h3 class="title"><?= $title; ?></h3>
-                <img src="<?= $img; ?>">
-                <p class="content"><?= $content; ?></p>
-                <p>Published: <?= $published_date; ?></p>
-                <p>Written by <?= $name; ?></p>
-                <p><?= $likes; ?></p>
-            </div>
-
-        </article>
-
-    <?php endforeach; ?>
 </body>
 
 </html>
